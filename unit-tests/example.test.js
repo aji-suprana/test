7ab94214e5d37f8158355db0e0497395b5e1f8c4
Aji <Suprana>
2020-07-19 12:42:16 +0700

    test

--- unit-tests/example.test.js
+++ unit-tests/example.test.js
@@ -18,3 +18,22 @@ test('adds 1 + 1',()=>{
   }
   expect(exampleRepo.sum(request)).toStrictEqual(response);
 })
+
+test('adds 1 + 2',()=>{
+  var request = 
+  {
+    body:{
+      a : 1,
+      b : 2
+    }
+  }
+
+  var response = 
+  {
+    'statusCode' : 200,
+    'status' : 'SUCCESS',
+    'data' : 2,
+    'message' : null
+  }
+  expect(exampleRepo.sum(request)).toStrictEqual(response);
+})
