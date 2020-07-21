a41ca5329b8b90787a52e5b55a429e40e96b7ebb
Aji <Suprana>
2020-07-22 04:18:18 +0700

    test123

--- config/CICD/CICD_config.js
+++ config/CICD/CICD_config.js
@@ -1,9 +1,12 @@
+var appName = process.env.APP_NAME;
+var dbName = appName +'-db';
+console.log(appName);
 module.exports=
 {
     "ECR_REPOSITORY": "my-ecr-repo",
     "IMAGE_TAG":
     {
-        'nodeApp':'vbs-authentication-app',
-        'db' : 'vbs-authentication-db'
+        'nodeApp':appName,
+        'db' : dbName
     }
 }
