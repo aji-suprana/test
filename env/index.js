a0edf2afefd131d75c5018ded86088d1d15cb50d
Aji <Suprana>
2020-07-21 17:31:18 +0700

    test1

--- env/index.js
+++ env/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const dotenv = require('dotenv');
 const debug = require('../app/services/debug')
 debug.logHeader("Setting up Environment")
-const envFile = { path: process.env.ENVIRONMENT?path.resolve(__dirname, `../env/${process.env.ENVIRONMENT}.env`):path.resolve(__dirname, `../env/.env`)};
+const envFile = { path: process.env.NODE_ENV?path.resolve(__dirname, `../env/${process.env.NODE_ENV}.env`):path.resolve(__dirname, `../env/.env`)};
 const envConfig = dotenv.config(envFile);
 debug.logData('env path', envFile.path)
 debug.logData('env config',envConfig)
