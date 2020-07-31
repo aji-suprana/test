955df12b211d7ff3e3be92ccf0a008b8e8fc8453
DESKTOP-95TJ6IL\Aji Suprana <ajiadiwenas@gmail.com>
2020-07-31 09:33:16 +0700

    merge conflict deployment

--- bin/CICD/template/deployment.js
+++ bin/CICD/template/deployment.js
@@ -8,26 +8,10 @@ constructJobs[uploadImage.name] = uploadImage.content;
 
 module.exports=
 {
-<<<<<<< HEAD
   "name": "Develop-CD",
   "on": {
     "push": {
       "branches": [
-=======
-  "on": 
-  {
-    "pull_request": 
-    {
-      "branches": 
-      [
-        "develop-stage",
-      ]
-    },
-    "push":
-    {
-      "branches": 
-      [
->>>>>>> 4ec7f2037b8bb214d4b27d9aac4db00135576e49
         "develop-stage",
       ]
     }
