b89b2f1b95f60b6abed37f3e29f4829d87d28351
DESKTOP-95TJ6IL\Aji Suprana <ajiadiwenas@gmail.com>
2020-07-31 09:53:29 +0700

    deployment to ecs

--- bin/CICD/template/deploy.js
+++ bin/CICD/template/deploy.js
@@ -1,10 +1,14 @@
 const buildSequelize = require('../jobs/built-test-sequelize')
 const uploadImage = require('../jobs/uploadImage')
+const deploy = require('../jobs/deploy')
+
 
 const constructJobs = {}
 
 constructJobs[buildSequelize.name] = buildSequelize.content;
 constructJobs[uploadImage.name] = uploadImage.content;
+constructJobs[deploy.name] = deploy.content;
+
 
 module.exports=
 {
